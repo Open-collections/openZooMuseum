5feb35a83cf96c6bde50c5d4b282f602c9cdb36c
Logan Wilt <91149982+MuchQuak@users.noreply.github.com>
2025-10-08 13:21:25 -0700

    Bugfix schema issues 3.4 from fresh install (#2970)
    * Add if exists check to storageAge drop
    
    * Seperate index drop and readd to be under disabled foreign key checks

--- config/schema/3.0/patches/db_schema_patch-3.4.sql
+++ config/schema/3.0/patches/db_schema_patch-3.4.sql
@@ -20,7 +20,7 @@ ALTER TABLE `omoccurpaleo`
   CHANGE COLUMN `geologicalContextID` `geologicalContextID` VARCHAR(100) NULL DEFAULT NULL ;
 
 ALTER TABLE `omoccurpaleo`
-  DROP COLUMN `storageAge`;
+  DROP COLUMN IF EXISTS `storageAge`;
 
 #Add paleo indexes
 ALTER TABLE `omoccurpaleo`
@@ -45,13 +45,20 @@ ALTER TABLE `omoccurpaleogts`
 
 ALTER TABLE `omoccurpaleogts`
   DROP INDEX `UNIQUE_gtsterm`,
-  DROP INDEX `FK_gtsparent_idx`;
 
 ALTER TABLE `omoccurpaleogts`
   ADD UNIQUE INDEX `UQ_paleogts_gtsterm` (`gtsterm` ASC),
-  ADD INDEX `FK_paleogts_parent_idx` (`parentGtsID` ASC),
   ADD INDEX `IX_paleogts_myaStart` (`myaStart` ASC),
   ADD INDEX `IX_paleogts_myaEnd` (`myaEnd` ASC);
+
+# Disable foreign key checks temporarily to rename index
+SET FOREIGN_KEY_CHECKS=0;
+
+ALTER TABLE `omoccurpaleogts`
+  DROP INDEX IF EXISTS `FK_gtsparent_idx`,
+  ADD INDEX IF NOT EXISTS `FK_paleogts_parent_idx` (`parentGtsID` ASC);
+
+SET FOREIGN_KEY_CHECKS=1;
 
 #reset the values within omoccurpaleogts table
 TRUNCATE `omoccurpaleogts`;
